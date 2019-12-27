94783482652c73e5698ecc7da89c77dc231e186b
hjj <547784390@qq.com>
2019-12-27 12:11:59 +0800

    第二次提交

--- src/cn/accp/HelloGit.java
+++ src/cn/accp/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
         System.out.println("你的家乡还好吗？");
         System.out.println("不好！");
         System.out.println("很好！");
+        System.out.println("我的家乡很美");
     }
 }
